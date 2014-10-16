57561443a5db77d67d3f3b9c402b85bdf406165f
Jon Flanders <jon.flanders@openx.com>
2014-10-15 17:08:23 -0700

    adding models

---
+++ iOSMazeClient/Maze.swift
@@ -0,0 +1,14 @@
+//
+//  Maze.swift
+//  iOSMazeClient
+//
+//  Created by Jon Flanders on 10/15/14.
+//  Copyright (c) 2014 Flounderware. All rights reserved.
+//
+
+import Foundation
+
+
+struct Maze {
+    
+}
---
+++ iOSMazeClient/Mazes.swift
@@ -0,0 +1,14 @@
+//
+//  Mazes.swift
+//  iOSMazeClient
+//
+//  Created by Jon Flanders on 10/15/14.
+//  Copyright (c) 2014 Flounderware. All rights reserved.
+//
+
+import Foundation
+
+
+struct Mazes {
+    
+}
